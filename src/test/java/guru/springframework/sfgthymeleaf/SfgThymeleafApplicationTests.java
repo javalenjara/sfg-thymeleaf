c9968496ddde793e0b68fce23e4d11710fa6055b
javalenjara <Javalen git2016>
2020-09-16 17:43:16 -0500

    Initial commit

---
+++ src/test/java/guru/springframework/sfgthymeleaf/SfgThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
